70e59fd074eef4e352c2654befb877293e6a7bec
Samuel Miller <smillercb@gmail.com>
2022-11-11 02:52:31 -0500

    learningFirestore

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,29 +5,32 @@ import { selectUser } from "../features/counter/userSlice";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import db from "../firebase";
-import { collection, CollectionReference, doc, DocumentSnapshot, QuerySnapshot } from "firebase/firestore";
+import { collection, CollectionReference, doc, DocumentSnapshot, getDoc, query, where,getDocs } from "firebase/firestore";
 import { useState } from "react";
 
 const Profile = () => {
 
-    const [products,setProducts]=useState();
+    const [products, setProducts] = useState();
 
     const user = useSelector(selectUser)
 
-    useEffect(()=>{
-    
-        collection(db,"products")
-        
-        console.log(allProducts);
-        
-    },[])
+    useEffect(() => {
+        const q = query(collection(db, "products"), where("active", "==", true))
+        getDocs(q)
+        .then(querySnapshot=>{
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+        })
+        });
+
+    }, [])
 
 
     return (
         <div className="profile">
             <Navbar></Navbar>
             <div className="profileBody">
-                <h1 style={{ "fontSize": "30pt" ,color:"white" }}>Edit Profile</h1>
+                <h1 style={{ "fontSize": "30pt", color: "white" }}>Edit Profile</h1>
                 <div className="border"></div>
                 <div className="dflex">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" className="avatar2" alt="ProfileLogo" />
